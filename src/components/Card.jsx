3c58087d3cdce0e4a10bc84252e87cc8c954325a
Jagadheesh-007 <jagadheesh2005@gmail.com>
2026-01-04 23:55:17 +0530

    Sample Commit Testing ...

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 const Card = ({title,desc,img,pid}) => {
   return (
     <>
+
     <div className='h-[300px] w-[300px] flex flex-row shadow-md shadow-black'>
         <div className="h-[150px] w-full"><img src={img}/></div>
         <div className="h-[50px] w-full"><p>{title}</p></div>
